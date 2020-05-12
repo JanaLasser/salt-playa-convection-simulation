a3e11583cc307ae7243059d72b152ec5620844e8
Jana <jana.lasser@ds.mpg.de>
2020-05-12 18:57:40 +0200

    current version of testing routine

--- testing-routine.sh
+++ testing-routine.sh
@@ -1,1 +1,1 @@
-python saltplayaconvection_uy.py -dest testing/ -plt -H 10 -A 1 -res 16 -clf 0.05 -Ra 500 -T 1 -savetime 0.001 -S 1528880287
+python saltplayaconvection_uy.py -dest testing/ -plt -H 40 -L 40 -res 8 -clf 0.05 -Ra 100 -T 20 -savetime 0.01 -S 1528880287 -saveall -waves 15 -amplitude 1
