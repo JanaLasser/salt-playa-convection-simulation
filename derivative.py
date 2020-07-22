23937588d6a13fab5b9f1b5643c6f7d5d6197c42
Jana <jana.lasser@ds.mpg.de>
2020-07-22 21:23:55 +0200

    added header

--- derivative.py
+++ derivative.py
@@ -1,6 +1,47 @@
-### periodic derivative matrices ###
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Two-dimensional simulation code for buoyancy driven convection below an 
+evaporating salt lake. Allows for input of different simulation parameters
+(see main.py -h for all available options) as well as different boundary
+conditions.
+
+This source code is subject to the terms of the MIT license. If a copy of the
+MIT license was not distributed with this file, you can obtain one at
+https://opensource.org/licenses/MIT
+
+Permission is hereby granted, free of charge, to any person obtaining a copy 
+of this software and associated documentation files (the "Software"), to 
+deal in the Software without restriction, including without limitation the 
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+sell copies of the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in 
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+THE SOFTWARE.
+"""
+
+__author__ = 'Jana Lasser'
+__copyright__ = 'Copyright 2020, Geophysical pattern formation in salt playa'
+__credits__ = ['Jana Lasser', 'Marcel Ernst']
+__license__ = 'MIT'
+__version__ = '1.0.0'
+__maintainer__ = 'Jana Lasser'
+__email__ = 'lasser@csh.ac.at'
+__status__ = 'Dev'
 
 import numpy as np
+
+### periodic derivative matrices ###
 
 def CyclicDiagonalMatrix(entries, size):
 	width = len(entries)
